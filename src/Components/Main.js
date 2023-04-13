184d21f78597adb7e2d4d3f0f00dc153cfa9b61b
green-n <romunuk@gamil.com>
2023-04-13 23:19:21 +0300

    final for now

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,36 +1,74 @@
-import react ,{useState}from "react";
+import react ,{useState, useEffect}from "react";
 import Card from "./Card";
 import axios from "axios";
 const Main=()=>{
+    const optionalCategories = ["History","art","Biography","Computers","Medical","Poetry"]
     const [search,setSearch]=useState("");
     const [bookData,setData]=useState([]);
-    const searchBook=(evt)=>{
-        if(evt.key==="Enter")
-        {
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
-            .then(res=>setData(res.data.items))
-            .catch(err=>console.log(err))
-        }
+    const [sortMethod,setSortMethod]=useState("");
+    const [categorie,setCategorie]= useState("") 
+    const findBook = () =>{
+        axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'+intitle+&orderBy=' + sortMethod + '&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
+        .then(res=>{setData(res.data.items)
+        console.log(res)})
+        .catch(err=>console.log(err))
     }
+
+    const formHandler = (e) =>{
+        e.preventDefault();
+        setSearch(e.target.bookName.value)
+        setSortMethod(e.target.order.value)
+        setCategorie(e.target.Categories.value)
+    }
+
+    useEffect(() => {
+        findBook();
+    }, [search, sortMethod, categorie]);
+
+
     return(
         <>
             <div className="header">
-                <div className="row1">
-                </div>
-                <div className="row2">
-                    <h2>Find Your Book</h2>
-                    <div className="search">
-                        <input type="text" placeholder="Enter Your Book Name"
+                    <div >
+                        {/* <input type="text" placeholder="Enter Your Book Name"
                         value={search} onChange={e=>setSearch(e.target.value)}
                         onKeyPress={searchBook}/>
-                        <button><i className="fas fa-search"></i></button>
+                        <button><i className="fas fa-search"></i></button> */}
+                        <form onSubmit={formHandler} >
+                            <label>
+                                Book Name 
+                                <input
+                                name="bookName"
+                                type="text"
+                                required />
+                            </label>
+                            <label>
+                                order:
+                                <select
+                                name="order"
+                                required>
+                                <option key="relevance">relevance</option>
+                                <option key="newest">newest</option>
+                                </select>
+                            </label>
+                            <label>
+                                Categories:
+                                <select
+                                name="Categories">
+                                <option key=""></option>
+                                {optionalCategories.map(el =>(<option key={el}>{el}</option>))}
+                                </select>
+                            </label>
+                            <button>Find Book</button>
+                        </form>
                     </div>
                 </div>
-            </div>
 
             <div className="container">
               {
-                    <Card book={bookData}/>
+                    <Card book={bookData.filter(el => {
+                        return categorie === "" || el.volumeInfo.categories == categorie                
+                })}/>
               }  
             </div>
         </>
